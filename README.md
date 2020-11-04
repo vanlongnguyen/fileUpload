882de44b751c3d9836e83142b4789a010926b51b
Mac <vanlong2507@gmail.com>
2020-11-04 22:00:34 +0800

    Story 2 & 3

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # upload_file
+1. git clone git@github.com:vanlongnguyen/upload_file.git
+2. sudo pip3 install virtualenv
+3. cd /upload_file
+3. source bin/activate
+4. run python.main.py
+
+#end points
+
+1. Upload files
+	http://localhost:5000/uploader
+	Images are uploaded in assests folder
+2. Generate thumbnails from uploaded images
+	http://localhost:5000/generate-thumbnails
+	Images are generated to thumbnails_generated folder
--- main.py
+++ main.py
@@ -1,42 +1,117 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from flask_restful import Resource, Api
+from zipfile import ZipFile 
 import os
 import sys
+import re
+import shutil
+from PIL import Image
+import copy
+
 
 app = Flask(__name__)
 api = Api(app)
-UPLOAD_DIRECTORY = "uploads"
-app.config['UPLOAD_FOLDER'] = UPLOAD_DIRECTORY
+store_path = 'assests'
+tmp_path = 'tmp/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'zip'}
-
+ALLOWED_RENAMED = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1].lower()
 
+
+def rename_files():
+	if not os.path.isdir(store_path):
+		os.mkdir(store_path)
+	for file in os.listdir(tmp_path):
+		if allowed_file(file) in ALLOWED_RENAMED:
+			new_store_path = tmp_path + '/'
+			os.rename(tmp_path + file, store_path + '/' +  file.replace(' ', '_'))
+	return 'renamed'
+
+def clean_up():
+	if os.path.isdir(tmp_path):
+		shutil.rmtree(tmp_path)
+		return 'cleaned tmp folder'
+
+def do_generate_image(files, thumbnail_des, file, img_origin, extra_name, size):
+	filename = file.rsplit('.', 1)[0].lower()
+	filetype = file.rsplit('.', 1)[1].lower()
+
+	img = copy.deepcopy(img_origin)
+
+	img.thumbnail(size)
+	thumb = thumbnail_des + '/' + filename + extra_name + '.' + filetype
+	img.save(thumb, filetype)
+	files.append(thumb)
+
+# LIST UPLOADTED FILES
+@app.route("/files", methods = ['GET'])
+def list_files():
+	clean_up()
+	files = []
+	for file in os.listdir(store_path):
+		files.append(os.path.abspath(store_path + '/' + file))
+	return jsonify(files)
+
+
+
+# GENERAT THUMBNAILS
+@app.route("/generate-thumbnails", methods = ['GET'])
+def generate_thumbnail():
+	size_32 = 32, 32
+	size_64 = 64, 64
+	origin = '_original'
+
+	files = []
+
+	thumbnail_des = os.path.abspath('thumbnails_generated')
+
+	if not os.path.isdir(thumbnail_des):
+		os.mkdir(thumbnail_des)
+	for file in os.listdir(store_path):
+		img_origin = Image.open(os.path.abspath(store_path+'/'+file))
+		w, h = img_origin.size
+		
+		if w >= 128 and h >= 128:
+			do_generate_image(files, thumbnail_des, file, img_origin, '_32' , size_32)
+
+			do_generate_image(files, thumbnail_des, file, img_origin, '_64', size_64)
+		else:
+			do_generate_image(files, thumbnail_des, file, img_origin, origin , img_origin.size)
+
+	return 'Generate thumbnails from original images successfully \n' + str(files)
+
+
+# UPLOADER
 @app.route('/uploader', methods = ['GET', 'POST'])
 
 def upload_file():
 	if request.method == 'POST':
-		file = request.files['file']
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return filename + ' is uploaded successfully'
-		else:
-			message = ', ' .join(ALLOWED_EXTENSIONS)
-			return 'Format is invalid to upload, only except: ' + message
+		try:
+			file = request.files['file']
+			if not os.path.isdir(tmp_path):
+				os.mkdir(tmp_path)
+			if file and allowed_file(file.filename) in ALLOWED_EXTENSIONS:
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(tmp_path, filename))
+				path = os.path.join(tmp_path, filename)
+				if os.path.isfile(path) and allowed_file(filename) == 'zip':
+					with ZipFile(os.path.join(tmp_path, file.filename), 'r') as zip_ref:
+						zip_ref.printdir()
+						zip_ref.extractall(tmp_path)
+				else:
+					rename_files()
+					return os.path.abspath(store_path + '/'+ filename)
+				rename_files()
+				return list_files()
+			else:
+				message = ', ' .join(ALLOWED_EXTENSIONS)
+				return 'Format is invalid to upload, only except: ' + message
+		except Exception as e:
+			return str(e)
 
-@app.route("/files", methods = ['GET'])
-
-def list_files():
-	files = []
-	for filename in os.listdir(UPLOAD_DIRECTORY):
-		path = os.path.join(UPLOAD_DIRECTORY, filename)
-		if os.path.isfile(path):
-			abs_path = os.path.abspath(filename)
-			files.append(abs_path)
-	return jsonify(files)
 
 if __name__ == '__main__':
 	app.run(debug=True)
